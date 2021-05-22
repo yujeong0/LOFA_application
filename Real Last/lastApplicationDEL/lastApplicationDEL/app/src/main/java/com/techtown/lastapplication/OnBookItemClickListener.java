3fd8c65fe88822ef13b3a7da614e356ef9901767
강유정 <kos9811@naver.com>
2021-05-22 11:38:42 +0900

    정리

--- Real Last/lastApplicationDEL/lastApplicationDEL/app/src/main/java/com/techtown/lastapplication/OnBookItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.techtown.lastapplication;
-
-import android.view.View;
-
-public interface OnBookItemClickListener {
-    public void onItemClick(BookAdapter.ViewHolder holder, View view, int position);
-}
--- Real Last/lastApplicationDEL/lastApplicationDEL/app/src/main/java/com/techtown/lastapplication/OnDatabaseCallback.java
+++
@@ -1,9 +0,0 @@
-package com.techtown.lastapplication;
-
-import java.util.ArrayList;
-
-public interface OnDatabaseCallback {
-    public void insert(int number, String name, String author, String contents, double lat, double lon, byte[] photo);
-    public void delete(String title);
-    public ArrayList<BookInfo> selectAll();
-}
